58f10185933f4c0021439d539eb88257ac80d6b4
felipe <felipe.faule@gmail.com>
2026-07-27 11:08:33 -0300

    Cambiamos de nombre la app de nacho a tiendalibre

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from nacho.views import ProductosTemplateView
+from tiendalibre.views import ProductosTemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
